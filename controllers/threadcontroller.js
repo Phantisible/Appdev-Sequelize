4a8c9f1dfb1348cb2db8fe497503773deb07d59f
mariahariannie <82221345+mariahariannie@users.noreply.github.com>
2021-05-27 23:19:34 +0800

    Update threadcontroller.js

--- controllers/threadcontroller.js
+++ controllers/threadcontroller.js
@@ -17,3 +17,14 @@ exports.addThread = async (req, res) => {
     res.render("home");
     
 }
+
+exports.updateThread = async (req, res) => {
+    res.locals.user = req.session.user;
+    await threads.model.update({
+        threaduuid : req.session.user.uuid,
+    },req.body,
+    {new: true},
+    ).exec();
+    req.flash('success', 'Successfully updated');
+    res.redirect('home');
+}
