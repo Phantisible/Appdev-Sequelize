899aa2eed6de4f7a03f26887ca60cf7c86c431b8
mariahariannie <82221345+mariahariannie@users.noreply.github.com>
2021-06-01 23:37:06 +0800

    Update threadroutes.js

--- routes/threadroutes.js
+++ routes/threadroutes.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const threadcontroller = require ("../controllers/threadcontroller");
+const commentcontroller = require ("../controllers/commentcontroller");
 
+//thread
 router.get("/threadList", threadcontroller.threadList);
 router.post("/addThread", threadcontroller.addThread);
+router.post("/updateThread", threadcontroller.updateThread);
+
+//comments
+router.post("/createComment", commentcontroller.createComment);
 
 module.exports = router;
